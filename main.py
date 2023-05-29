01cf1fde58c4a163c935092f921c1eac8957f67b
Mariana Navarro <70180396+NavarroMarian@users.noreply.github.com>
2023-05-29 18:34:09 -0300

    objetivo

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+""" 
+Objetivo: El Trabajo Práctico consistirá en el desarrollo de un programa Python que responda a la consigna indicada a continuación.
+En el mismo deberán aplicarse las técnicas de programación tratadas en la materia y detalladas más abajo:FuncionesMatricesCadenas de
+caracteresExcepcionesArchivosRecursividadTuplas, conjuntos y/o diccionarios (uno de los tres será suficiente).Todos estos temas
+tendrán que ser incluidos en el programa. La ausencia de alguno de ellos reducirá significativamente la calificación.
+Los temas podrán ser añadidos a medida que sean abordados en clase. Nota: No se permite el uso de módulos externos no 
+tratados en clase como pygame, freegames, numpy, tkinter, etc. Sin embargo, los módulos time, os y colorama pueden ser
+utilizados si fuera necesario.
+""" 
